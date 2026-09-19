3db1252a811dc3258cf8b629b6df331476f401aa
Safaa-khalil <safaa.khalil@msa.edu.eg>
2026-09-19 18:02:48 +0300

    FinalExam

--- Finalexam.cs
+++ Finalexam.cs
@@ -7,17 +7,17 @@ using System.Threading.Tasks;
 namespace EXAM
 {
     #region FinalExam
-    public class FinalExam : Exam
-    {
-        public FinalExam(TimeSpan time, List<Question> questions) : base(time, questions)
-        {
-        }
+    //public class FinalExam : Exam
+    //{
+    //    public FinalExam(TimeSpan time, List<Question> questions) : base(time, questions)
+    //    {
+    //    }
 
-        public override void ShowExam()
-        {
-            PresentAllQuestions("Final Exam");
-            ShowResults("Final Exam Results:");
-        }
-    }
+    //    public override void ShowExam()
+    //    {
+    //        PresentAllQuestions("Final Exam");
+    //        ShowResults("Final Exam Results:");
+    //    }
+    //}
     #endregion
 }
