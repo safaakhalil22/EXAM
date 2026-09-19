c3a3f512f8dc5c50d1bd330db032f108dd79bd95
Safaa-khalil <safaa.khalil@msa.edu.eg>
2026-09-19 18:03:28 +0300

    MCQQuestion

--- Mcqquestion.cs
+++ Mcqquestion.cs
@@ -7,14 +7,14 @@ using System.Threading.Tasks;
 namespace EXAM
 {
     #region MCQQuestion
-    public class MCQQuestion : Question
-    {
-        public override string TypeLabel => "MCQ";
+    //public class MCQQuestion : Question
+    //{
+    //    public override string TypeLabel => "MCQ";
 
-        public MCQQuestion(string header, string body, int mark, List<Answer> answerList, Answer rightAnswer)
-            : base(header, body, mark, answerList, rightAnswer)
-        {
-        }
-    }
+    //    public MCQQuestion(string header, string body, int mark, List<Answer> answerList, Answer rightAnswer)
+    //        : base(header, body, mark, answerList, rightAnswer)
+    //    {
+    //    }
+    //}
     #endregion
 }
